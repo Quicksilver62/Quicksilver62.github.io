209e77f1aaba2ba15f45f1aa15337119bc0fc074
Quicksilver62 <49649193+Quicksilver62@users.noreply.github.com>
2019-04-23 17:08:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,8 +16,15 @@ function handler() {
 	layer.style.display = "flex";
 }
 
-var list = document.getElementsByClassName("drop_down")[0];
-list.onclick = function (e) {
 
-}
+var panelItem = document.querySelectorAll('.drop_down'),
+  active = document.getElementsByClassName('panel-active');
 
+Array.from(panelItem).forEach(function(item, i, panelItem) {
+	item.addEventListener('click', function(e) {
+		if (active.length > 0 && active[0].childNodes[3] !== this) { //если есть активный элемент, и это не тот по которому кликнули
+		active[0].classList.remove('panel-active'); };// убрать класс panel-active
+    // изменить состояние класса panel-active на текущем элементе: добавить если не было, убрать если было.
+	this.parentNode.classList.toggle('panel-active');
+  });
+});
