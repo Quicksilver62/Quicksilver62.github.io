864e165814446878fa21545e64bc40905d3f4381
Quicksilver62 <49649193+Quicksilver62@users.noreply.github.com>
2019-06-14 17:17:29 +0300

    Update script03.js

--- script03.js
+++ script03.js
@@ -5,7 +5,7 @@ var vh = window.innerHeight*0.01;
 var header = document.querySelector('header');
 header.style.setProperty('--vh', vh + 'px');
 
-window.addEventListener('scroll' ,function(){
+window.addEventListener('scroll', function(){
 	window.removeEventListener('resize', hendlerResize);
 //	debounce(window.addEventListener('resize', hendlerResize),2000);
 });
